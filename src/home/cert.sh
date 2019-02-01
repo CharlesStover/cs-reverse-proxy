086fb0eb4f3626f9705fa39edd4e3941c400fd5c
Charles Stover <github@charlesstover.com>
2019-01-31 23:46:48 -0800

    ignore certbot

--- src/home/cert.sh
+++ src/home/cert.sh
@@ -4,6 +4,8 @@ certbot certonly \
   --agree-tos \
   --domains $1 \
   --email certbot@charlesstover.com \
+  --renew-by-default \
   --rsa-key-size 4096 \
+  --text \
   --webroot \
   --webroot-path /var/www/letsencrypt
